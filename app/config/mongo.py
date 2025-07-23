81546e8e33742e8fb645a39fd1b67d9c7de6938a
unknown <kegonzalezca@est.utn.ac.cr>
2025-07-23 04:33:29 -0600

    some documentation lines

--- app/config/mongo.py
+++ app/config/mongo.py
@@ -1,14 +1,14 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
-# Puedes usar variables de entorno para manejar la URI (más seguro)
+# opcion: usar variables de entorno para manejar la URI (más seguro)
 MONGO_URI = os.getenv(
     "MONGO_URI",
     "mongodb+srv://utnuser:utnus3r24@cluster0.d9lhmxd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 )
 
-# Define aquí el nombre de la base de datos (puedes crearla desde MongoDB Atlas o la primera vez que insertes)
-DATABASE_NAME = "gym_management_db"  # ejemplo: gym_management_db
+# aquí se pasa el nombre de la base de datos (se puede crear desde MongoDB Atlas o la primera vez que inserte)
+DATABASE_NAME = "gym_management_db"  # gym_management_db
 
 client = AsyncIOMotorClient(MONGO_URI)
 database = client[DATABASE_NAME]
